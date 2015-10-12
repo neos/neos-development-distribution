e5253ee3ace919b41951cab53fdff821dfdff7d5
Aske Ertmann <aertmann@gmail.com>
2015-10-12 13:01:03 +0200

    [TASK] Adjust changelog script to Github pull requests
    See https://discuss.neos.io/t/change-log-and-github/530

--- Build/create-changelog.sh
+++ Build/create-changelog.sh
@@ -1,97 +1,105 @@
 #!/bin/bash
-
+set -e
 #
 # Generates a changelog in reStructuredText from the commit history of
 # the base distribution and the packages:
 #
+# - TYPO3.Media
 # - TYPO3.Neos
+# - TYPO3.Neos.Kickstarter
 # - TYPO3.Neos.NodeTypes
-# - TYPO3.SiteKickstarter
 # - TYPO3.TYPO3CR
 # - TYPO3.TypoScript
-# - TYPO3.NeosDemoTypo3Org
 #
 # Needs the following environment variables
 #
-# WORKSPACE        the base directory of the distribution
 # VERSION          the version that is "to be released"
 # PREVIOUS_VERSION the last released version, is guessed if not given
-# BRANCH           the branch that is worked on, used in commit message
-# BUILD_URL        used in commit message
+# BUILD_URL        used in commit message (optional)
 #
 
-cd "${WORKSPACE}"
-export TARGET="${WORKSPACE}/Packages/Application/TYPO3.Neos/Documentation/Appendixes/ChangeLogs/`echo ${VERSION} | tr -d .`.rst"
+if [ -z "$VERSION" ]; then echo "\$VERSION not set"; exit 1; fi
+if [ -z "$PREVIOUS_VERSION" ]; then echo "\$PREVIOUS_VERSION not set"; exit 1; fi
+
+cd Packages/Neos
+
+# Check for jq library
+hash jq 2>/dev/null || { echo >&2 "jq library is not installed. Aborting. Download at https://stedolan.github.io/jq/download/"; exit 1; }
+
+export TARGET="TYPO3.Neos/Documentation/Appendixes/ChangeLogs/`echo ${VERSION} | tr -d .`.rst"
+
+# Add version and date header
 export DATE="$(date +%Y-%m-%d)"
+echo "\`${VERSION} (${DATE}) <https://github.com/neos/neos-development-collection/releases/tag/${VERSION}>\`_" > ${TARGET}
+perl -E 'say "=" x '`echo $(($(tail -1 $TARGET | wc -c) - 1))` >> ${TARGET}
+echo -e "\nOverview of merged pull requests\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" >> ${TARGET}
 
-echo -e "==================\n${VERSION} (${DATE})\n==================\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nBase Distribution\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" > "${TARGET}"
-git log --pretty=format:"%s
------------------------------------------------------------------------------------------
+# Loop over merge commits since previous version
+for mergeCommit in `git log $PREVIOUS_VERSION.. --grep="Merge pull request" --oneline | cut -d ' ' -f1`; do
+	pullRequest=$(git show $mergeCommit --oneline | cut -d ' ' -f5 | cut -c2-)
+	if [ -z "$GITHUB_TOKEN" ];
+	then
+		curl https://api.github.com/repos/neos/neos-development-collection/pulls/$pullRequest > pr
+	else
+		curl https://api.github.com/repos/neos/neos-development-collection/pulls/$pullRequest?access_token=$GITHUB_TOKEN > pr
+	fi
+	if [[ $(cat pr | jq '.message') != "null" ]]; then cat pr | jq -r '.message'; exit 1; fi
+	echo "\`"`cat pr | jq -r '.title'`" <"https://github.com/neos/neos-development-collection/pull/$pullRequest">\`_" >> $TARGET
+	perl -E 'say "-" x '`echo $(($(tail -1 $TARGET | wc -c) - 1))` >> ${TARGET}
+	echo >> $TARGET
+	cat pr | jq -r '.body' >> $TARGET
+	echo >> $TARGET
+	packages=()
+	# Loop over changed files in merge commit
+	for changedFile in `git show $mergeCommit^ $mergeCommit^2 --name-only --oneline | tail -n +2`; do
+		# Get first part of changed filename
+		package=$(echo "$changedFile" | cut -d '/' -f1)
+		# Check if first part of filename is a directory
+		if [ -d "$package" ]; then
+			# Add last part of package key to packages array
+			packages=("${packages[@]}" $(echo $package | rev | cut -d '.' -f1 | rev))
+		fi
+	done
+	# Remove duplicates
+	packages=$(echo ${packages[@]} | tr ' ' '\n' | sort -u | tr '\n' ' ')
+	if [ -n "${packages// }" ]; then
+		echo "* Packages: \`\`"`echo $packages | sed 's/ /\`\` \`\`/g'`"\`\`" >> $TARGET
+	fi
+	echo >> $TARGET
+done
+# Remove pr file
+rm -f pr
 
-%b
-
-* Commit: \`%h <https://git.typo3.org/Neos/Distributions/Base.git/commit/%H>\`_
-
-" --no-merges ${PREVIOUS_VERSION}.. >> "${TARGET}"
-
-for PACKAGE in TYPO3.Neos TYPO3.Neos.NodeTypes TYPO3.Neos.Kickstarter TYPO3.TYPO3CR TYPO3.TypoScript TYPO3.Media ; do
-	cd "${WORKSPACE}/Packages/Application/${PACKAGE}"
-	echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n${PACKAGE}\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" >> "${TARGET}"
-	git log --pretty=format:"%s
------------------------------------------------------------------------------------------
-
-%b
-
-* Commit: \`%h <https://git.typo3.org/Packages/${PACKAGE}.git/commit/%H>\`_
-
-" --no-merges ${PREVIOUS_VERSION}.. >> "${TARGET}" || true
-done;
-
-cd "${WORKSPACE}/Packages/Sites/TYPO3.NeosDemoTypo3Org"
-echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nTYPO3.NeosDemoTypo3Org\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" >> "${TARGET}"
-git log --pretty=format:"%s
------------------------------------------------------------------------------------------
-
-%b
-
-* Commit: \`%h <https://git.typo3.org/Packages/TYPO3.NeosDemoTypo3Org.git/commit/%H>\`_
-
-" --no-merges ${PREVIOUS_VERSION}.. >> "${TARGET}" || true
-cd -
+echo -e "\n\n\`Detailed log <https://github.com/neos/neos-development-collection/compare/$PREVIOUS_VERSION...$VERSION>\`_" >> $TARGET
+perl -E 'say "~" x '`echo $(($(tail -1 $TARGET | wc -c) - 1))` >> ${TARGET}
 
 # Drop some footer lines from commit messages
-perl -p -i -e 's|^Change-Id: (I[a-f0-9]+)$||g' "${TARGET}"
-perl -p -i -e 's|^Releases?:.*$||g' "${TARGET}"
-perl -p -i -e 's|^Migration?:.*$||g' "${TARGET}"
-perl -p -i -e 's|^Reviewed-by?:.*$||g' "${TARGET}"
-perl -p -i -e 's|^Reviewed-on?:.*$||g' "${TARGET}"
-perl -p -i -e 's|^Tested-by?:.*$||g' "${TARGET}"
+perl -p -i -e 's|^Change-Id: (I[a-f0-9]+)$||g' ${TARGET}
+perl -p -i -e 's|^Releases?:.*$||g' ${TARGET}
+perl -p -i -e 's|^Migration?:.*$||g' ${TARGET}
+perl -p -i -e 's|^Reviewed-by?:.*$||g' ${TARGET}
+perl -p -i -e 's|^Reviewed-on?:.*$||g' ${TARGET}
+perl -p -i -e 's|^Tested-by?:.*$||g' ${TARGET}
 
-# Link issues to Forge
-perl -p -i -e 's/(Fixes|Resolves|Related|Relates|Extbase Issue): #([0-9]+)/* $1: `#$2 <http:\/\/forge.typo3.org\/issues\/$2>`_/g' "${TARGET}"
-perl -p -i -e 's/Security-Bulletin: (FLOW3-SA-[0-9]{4}-[0-9]+)/* Security-Bulletin: `$1 <http:\/\/typo3.org\/teams\/security\/security-bulletins\/flow3\/$1\/>`_/g' "${TARGET}"
-perl -p -i -e 's/Security-Bulletin: (FLOW-SA-[0-9]{4}-[0-9]+)/* Security-Bulletin: `$1 <http:\/\/typo3.org\/teams\/security\/security-bulletins\/flow\/$1\/>`_/g' "${TARGET}"
+# Link issues to Jira
+perl -p -i -e 's/(Fixes|Resolves|Related|Relates): NEOS-([0-9]+)/* $1: `NEOS-$2 <https:\/\/jira.neos.io\/browse\/NEOS-$2>`_/g' ${TARGET}
+
+# Link to commits
+perl -p -i -e 's/([0-9a-f]{40})/`$1 <https:\/\/github.com\/neos\/neos-development-collection\/commit\/$1>`_/g' ${TARGET}
 
 # escape backslashes
-perl -p -i -e 's/\\/\\\\/g' "${TARGET}"
+perl -p -i -e 's/\\/\\\\/g' ${TARGET}
 # clean up empty lines
-perl -p -i -0 -e 's/\n\n+/\n\n/g' "${TARGET}"
+perl -p -i -0 -e 's/\n\n+/\n\n/g' ${TARGET}
 # join bullet list items
-perl -p -i -0 -e 's/(\* [^\n]+)\n+(\* [^\n]+)/$1\n$2/g' "${TARGET}"
-# amend empty sections (todo: get rid of duplication)
-perl -p -i -0 -e 's/(~{40}\n[a-z0-9.]+\n~{40}\n)(\n~{40})/$1\nNo changes\n$2/ig' "${TARGET}"
-perl -p -i -0 -e 's/(~{40}\n[a-z0-9.]+\n~{40}\n)(\n~{40})/$1\nNo changes\n$2/ig' "${TARGET}"
-perl -p -i -0 -e 's/(~{40}\n[a-z0-9.]+\n~{40}\n)(\n*)$/$1\nNo changes\n$2/ig' "${TARGET}"
-# remove automatic submodule pointer raises
-perl -p -i -0 -e 's/\[TASK\] Raise submodule pointers(?:[^[]+)//ig' "${TARGET}"
-# remove automatic JS build commits
-perl -p -i -0 -e 's/Built minified JavaScript\/CSS files(?:[^[]+)//ig' "${TARGET}"
-
-# remove automatic lock file updates
-perl -p -i -0 -e 's/\[TASK\] Automatic composer.lock update(?:[^[]+)//ig' "${TARGET}"
+perl -p -i -0 -e 's/(\* [^\n]+)\n+(\* [^\n]+)/$1\n$2/g' ${TARGET}
 
 # commit generated changelog
-cd "${WORKSPACE}/Packages/Application/TYPO3.Neos"
-git add Documentation/Appendixes/ChangeLogs/`basename "${TARGET}"`
-git commit -m "[TASK] Add changelog for TYPO3 Neos ${VERSION}" -m "See $BUILD_URL" -m "Releases: $BRANCH"
+git add ${TARGET}
+if [ -z "$BUILD_URL" ]
+then
+	git commit -m "[TASK] Add changelog for ${VERSION}"
+else
+	git commit -m "[TASK] Add changelog for ${VERSION}" -m "See $BUILD_URL"
+fi
 cd -
