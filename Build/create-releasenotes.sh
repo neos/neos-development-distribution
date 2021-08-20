622bada6d7d82e24d4d5408fc22a1bf4e3bc1336
Karsten Dambekalns <karsten@dambekalns.de>
2021-08-20 10:23:59 +0200

    TASK: Align changelog & release notes creation to Flow

---
+++ Build/create-releasenotes.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+#
+# Generates a changelog in reStructuredText from the commit history of
+# the packages in Packages/Framework.
+#
+# Needs the following environment variables
+#
+# VERSION          the version that is "to be released", e.g "4.1.0"
+# PREVIOUS_VERSION the last released version, e.g. "4.0.0"
+# BUILD_URL        used in commit message (optional)
+# GITHUB_TOKEN     to authenticate github calls and avoid API limits
+#
+
+if [ -z "$VERSION" ]; then echo "\$VERSION not set"; exit 1; fi
+if [ -z "$PREVIOUS_VERSION" ]; then echo "\$PREVIOUS_VERSION not set"; exit 1; fi
+export TARGET="Neos.Neos/Documentation/Appendixes/ReleaseNotes/$(echo ${VERSION} | tr -d .).rst"
+
+php Build/BuildEssentials/build-tools.php neos:create-releasenotes neos $PREVIOUS_VERSION $VERSION $TARGET --filter=FEATURE --githubToken=$GITHUB_TOKEN --buildUrl=$BUILD_URL
