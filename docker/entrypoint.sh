4005563511ee6f85d0ae59fdf1816e56388ad35f
Robert Baruck <robert.baruck@sandstorm.de>
2023-05-08 11:31:22 +0200

    FEATURE: Add nginx, fpm and redis to docker dev setup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,20 +5,21 @@ composer install
 ./flow database:setcharset
 ./flow doctrine:migrate
 
-# only run site import when nothing was imported before
-importedSites=`./flow site:list`
+# only run site import when no site is present
+importedSites=$(./flow site:list)
 if [ "$importedSites" = "No sites available" ]; then
     echo "Importing content from Demo"
     ./flow site:import --package-key="Neos.Demo"
 fi
 
-./flow user:create --roles Administrator $ADMIN_USERNAME $ADMIN_PASSWORD LocalDev Admin || true
+./flow user:create --roles Administrator "$ADMIN_USERNAME" "$ADMIN_PASSWORD" LocalDev Admin || true
 
 ./flow resource:publish
 ./flow flow:cache:flush
 ./flow cache:warmup
 
-./flow server:run --host 0.0.0.0
-# e2e test
-#./flow behat:setup
-#rm bin/selenium-server.jar # we do not need this
+# start nginx in background
+nginx &
+
+# start PHP-FPM
+exec /usr/local/sbin/php-fpm
